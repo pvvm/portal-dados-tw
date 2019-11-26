e414e35f708a77a3ba3bc6c93fe45ed8a724bca6
Guilhermesfl <gsfl3101@gmail.com>
2019-11-26 16:47:52 -0300

    :sparkles: Added scaffold for the project and inital views

--- portal/urls.py
+++ portal/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.tweets_list, name='tweets_list'),
+  path('', views.home, name='home'),
+  path('list', views.tweets_list, name='tweets_list'),
+  path('search', views.tweets_search, name='tweets_search'),
+  path('articles', views.articles, name='articles'),
+  path('portals', views.portals, name='portals'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -3,8 +3,20 @@ from .models import Tweet
 import requests
 # Create your views here.
 def tweets_list(request):
-  pokemons = requests.get('http://localhost:3333/')
+  # pokemons = requests.get('http://localhost:3333/')
   tweets = []
-  for tweet in pokemons.json():
-    tweets.append(tweet['text'])
+  # for tweet in pokemons.json():
+  #   tweets.append(tweet['text'])
   return render(request, 'portal/tweets_list.html', {'tweets': tweets})
+
+def home(request):
+  return render(request, 'portal/home.html')
+
+def articles(request):
+  return render(request, 'portal/articles.html')
+
+def portals(request):
+  return render(request, 'portal/portals.html')
+
+def tweets_search(request):
+  return render(request, 'portal/tweets_search.html')
