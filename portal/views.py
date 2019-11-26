dc37212e3fc7f87e02f501f0396fb8f00e154cbc
Guilhermesfl <gsfl3101@gmail.com>
2019-11-26 02:07:48 -0300

    :sparkles: Added call to get tweets from scroller

--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Tweet
+import requests
 # Create your views here.
 def tweets_list(request):
-  tweets = Tweet.objects.all()
+  pokemons = requests.get('http://localhost:3333/')
+  tweets = []
+  for tweet in pokemons.json():
+    tweets.append(tweet['text'])
   return render(request, 'portal/tweets_list.html', {'tweets': tweets})
