73990c7c88f9e0c5f7c53a7e9a1da808582a7829
Guilhermesfl <gsfl3101@gmail.com>
2019-11-25 23:58:42 -0300

    :sparkles: Added tweets database response to template

--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Tweet
 # Create your views here.
 def tweets_list(request):
-  return render(request, 'portal/tweets_list.html', {})
+  tweets = Tweet.objects.all()
+  return render(request, 'portal/tweets_list.html', {'tweets': tweets})
